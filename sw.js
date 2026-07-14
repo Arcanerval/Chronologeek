0b558a04b0484a4b9b2b4650db71288965e43f02
Arcanerval <n.prades@yahoo.fr>
2026-07-14 14:56:01 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 const CACHE = 'cg-app';
 const IMG_CACHE = 'cg-img';
 const PRECACHE = [
-  '/', '/index.html', '/starwars.html', '/marvel.html', '/dc.html', '/avatar.html',
+  '/', '/index.html', '/starwars.html', '/marvel.html', '/dc.html', '/avatar.html', '/nouveautes.html',
   '/badges.js', '/pwa.js', '/manifest.json'
 ];
 
