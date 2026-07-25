0811ace59a6465cd6109c492647b58818b3623f1
Arcanerval <n.prades@yahoo.fr>
2026-07-25 18:16:31 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const IMG_CACHE = 'cg-img';
 const PRECACHE = [
   '/', '/index.html', '/starwars.html', '/marvel.html', '/dc.html', '/avatar.html', '/whats-new.html', '/upcoming.html',
   '/badges.js', '/pwa.js', '/anim.js', '/manifest.json',
-  '/progress.css', '/progress.js'
+  '/progress.css', '/progress.js', '/intro.css'
 ];
 
 self.addEventListener('install', e => {
